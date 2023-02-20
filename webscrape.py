2cc8de35dddabce1f50aaaa064c1f7dfffb90750
quiracle <jsalattorres@gmail.com>
2023-02-20 09:43:59 +0100

    Parse html

--- webscrape.py
+++ webscrape.py
@@ -14,10 +14,15 @@ class WebScrape(object):
 
     def parse_bs4(self):
         soup = bs4.BeautifulSoup(self.html, features = "html.parser")
-        div_main = soup.find_all("div", attrs=("id";"main"))
+        div_main = soup.find_all('div', attrs=('id';'main'))[0]
+        self.data = div_main.find_all('pre')[0]
+
+    def extract_data(self):
+        self.data = self.data.text
 
     def parse_html(self):
         self.parse_bs4()
+        self.extract_data()
 
     def get_data(self):
         self.get_web()
